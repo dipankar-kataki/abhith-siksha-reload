7f19ec5c1b06fe4f12b88a1983d0f795b6af1d24
dipankar kataki <dipankar.kataki@ekodus.com>
2023-06-21 14:37:06 +0530

    abhith new logo added for admin also

--- resources/views/layout/admin/include/navbar.blade.php
+++ resources/views/layout/admin/include/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-      <a class="navbar-brand brand-logo" href="{{route('admin.dashboard')}}"><img src="{{asset('asset_website/img/home/logo_.png')}}" alt="logo"/></a>
-      <a class="navbar-brand brand-logo-mini" href="{{route('admin.dashboard')}}"><img src="{{asset('asset_admin/images/logo-mini.svg')}}" alt="logo" /></a>
+      <a class="navbar-brand brand-logo" href="{{route('admin.dashboard')}}"><img src="{{asset('asset_website/img/home/abhith-reloaded.png')}}" alt="logo" style="height:40px;"/></a>
+      <a class="navbar-brand brand-logo-mini" href="{{route('admin.dashboard')}}"><img src="{{asset('asset_website/img/home/abhith-reloaded-mini.png')}}" alt="logo" style="height:40px;width:50px;" /></a>
     </div>
     <div class="navbar-menu-wrapper d-flex align-items-stretch">
       <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
