73de87035980be0ec410f4f993fa9cbd45508131
dipankar kataki <dipankar.kataki@ekodusinc.com>
2023-08-01 16:16:22 +0530

    env file changed abhith siksha

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,14 +17,13 @@ class UserSeeder extends Seeder
     public function run()
     {
         $data=[
-            'firstname' => Str::random(10),
-            'lastname' => Str::random(10),
+            'name' => Str::random(10).''.Str::random(10),
             'email' => 'teacher@gmail.com',
             'phone' =>8867589870,
             'otp'=>123456,
             'verify_otp'=>1,
-            'type_id'=>2,
-            'password' => Hash::make('123456teacher'),
+            'type_id'=>3,
+            'password' => Hash::make('123456'),
             'is_activate'=>1, 
         ];
         $user=User::create($data);
